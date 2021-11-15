bf55fa6f2d37f42e3ec60408893a86c69ada705e
Tomás Gonçalves <tomasfgon@gmail.com>
2021-11-15 14:42:00 +0000

    more analysis functions

--- data-analysis.py
+++ data-analysis.py
@@ -75,8 +75,6 @@ def moviesPerLanguage():
 
     return dict(sorted(word_counts.items(), key=lambda item: item[1], reverse=True))
     
-
-
 def plotMoviesPerLanguage():
     data_dict = (moviesPerLanguage())
     del data_dict["N/A"]
@@ -110,7 +108,7 @@ def mostCommonPlotWords():
     #number of words to show
     number_of_words_to_display = 200
 
-    stopwords = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+    stopwords = ['',"","i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
     
     words = []
 
@@ -156,6 +154,35 @@ def mostCommonPlotWords():
 
 
     return dict(itertools.islice(word_counts.items(), number_of_words_to_display))
+
+def plotMostCommonPlotWords():
+    data_dict = mostCommonPlotWords()
+    data_dict = dict(itertools.islice(data_dict.items(), 30))
+
+
+    courses = list(data_dict.keys())
+    values = list(data_dict.values())
+    fig = plt.figure(figsize = (29, 7))
+    #  Bar plot
+    plt.yticks(np.arange(0, max(values)+1, 500.0))
+    
+    plt.bar(courses, values, color ='green',
+            width = 0.5)
+
+    bars = plt.bar(courses, height=values, width=.5)
+
+    # access the bar attributes to place the text in the appropriate location
+    for bar in bars:
+        yval = bar.get_height()
+        plt.text(bar.get_x(), yval + .005, yval)
+
+    plt.xlabel("Words")
+    plt.ylabel("Occurrences")
+    plt.xticks(fontsize=7)
+    plt.title("Most used words in movie plots")
+   
+    plt.show()
+
 
 def getAverageRuntime():
     rootdir = "movies"
@@ -204,7 +231,7 @@ def getAvgDecade():
 
     return decade_counter
 
-def plotDecadeCounter():
+def plotAvgDecade():
     decade_counter = getAvgDecade()
 
     courses = list(decade_counter.keys())
@@ -230,6 +257,4 @@ def plotDecadeCounter():
     plt.title("Number of movies per decade")
     plt.show()
 
-            
-
-
+plotMostCommonPlotWords()
