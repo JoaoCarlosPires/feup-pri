3d8630211e15c99a2ab370805627cebf59d2aaf6
Tomás Gonçalves <tomasfgon@gmail.com>
2021-11-15 11:57:28 +0000

    more data-analysis

--- data-analysis.py
+++ data-analysis.py
@@ -157,4 +157,79 @@ def mostCommonPlotWords():
 
     return dict(itertools.islice(word_counts.items(), number_of_words_to_display))
 
-print(mostCommonPlotWords())
+def getAverageRuntime():
+    rootdir = "movies"
+
+    
+    addition = 0
+    movieCounter = 0
+
+    
+    for subdir, dirs, files in os.walk(rootdir):
+        if(files == ['details.txt', 'omdbContent.txt']):
+        
+            f = open(subdir + '/'+files[1], 'r')
+            data=f.read()
+            obj = json.loads(data)
+            minute = (obj['Runtime'].split()[0])
+            if minute.isnumeric():
+                addition = addition + int((obj['Runtime'].split()[0]))
+                movieCounter = movieCounter + 1
+            
+            f.close()
+
+    return addition/movieCounter
+
+def getAvgDecade():
+
+
+    rootdir = "movies"
+    list=[]
+
+    for subdir, dirs, files in os.walk(rootdir):
+        if(files == ['details.txt', 'omdbContent.txt']):
+        
+            f = open(subdir + '/'+files[1], 'r')
+            data=f.read()
+            obj = json.loads(data)
+            
+            decade = obj["Released"][-4:]
+            if decade.isnumeric():
+                decade = int(decade)
+                decade = (decade//10) * 10
+                list.append(decade)
+    decade_counter = dict(Counter(list))
+
+    decade_counter = dict(sorted(decade_counter.items(), key=lambda item: item[1], reverse=True))
+
+    return decade_counter
+
+def plotDecadeCounter():
+    decade_counter = getAvgDecade()
+
+    courses = list(decade_counter.keys())
+    values = list(decade_counter.values())
+    fig = plt.figure(figsize = (10, 5))
+    #  Bar plot
+    plt.yticks(np.arange(0, max(values)+1, 500.0))
+    
+    
+    plt.bar(courses, values, color ='green',
+            width = 0.5)
+
+    bars = plt.bar(courses, height=values, width=.5)
+    
+
+    # access the bar attributes to place the text in the appropriate location
+    for bar in bars:
+        yval = bar.get_height()
+        plt.text(bar.get_x(), yval + .5, yval)
+
+    plt.xlabel("Decades")
+    plt.ylabel("Number of movies")
+    plt.title("Number of movies per decade")
+    plt.show()
+
+            
+
+
