908a08df5951f79bafe8b79cc7a2f13fe0e2daf7
Tomás Gonçalves <tomasfgon@gmail.com>
2021-12-07 17:48:08 +0000

    new data.json

--- HandleContent.py
+++ HandleContent.py
@@ -2,8 +2,9 @@ import json
 import os
 from typing_extensions import final
 
-finalData = {}
+finalData = []
 data = {}
+
 
 class doubleQuoteDict(dict):
     def __str__(self):
@@ -15,26 +16,23 @@ class doubleQuoteDict(dict):
 
 def getMovieContent(folder):
 
-    moviefoldername = folder.replace("movies/", "")
-    data[moviefoldername] = {}
+    moviefoldername = folder.replace("Projeto/movies/", "")
+   
+    data["id"] = str(moviefoldername)
 
-    omdb_txt = ["Title", "Rated", "Release", "Runtime", "Genre", "Director", "Writer", "Actors", "Plot", "Language", "Country", "Awards", "Poster", "Ratings", "Metascore", "imdbRating", "imdbVotes", "DVD", "BoxOffice", "Production", "Website"]
+    omdb_txt = ["Title", "Rated", "Release", "Runtime", "Genre", "Director", "Writer", "Actors", "Plot", "Language", "Country", "Awards", "Poster", "Metascore", "imdbRating", "imdbVotes", "DVD", "BoxOffice", "Production", "Website"]
     tmdb_txt = ["overview", "popularity", "poster_path", "video", "vote_average", "vote_count"]
 
 
     
-
-
-        
-
     if(os.path.isfile(folder+"/omdbContent.txt")):
         with open(folder+"/omdbContent.txt", 'r') as handle:
                 parsed = json.load(handle)
                 for attr in omdb_txt:
                     if attr in parsed:
-                        data[moviefoldername][(attr)] = (parsed[attr])
+                        data[(attr)] = (parsed[attr])
                     else:
-                        data[moviefoldername][(attr)] = "N/A"
+                        data[(attr)] = "N/A"
         handle.close()
 
                 
@@ -48,9 +46,9 @@ def getMovieContent(folder):
                 parsed = json.load(handle)
                 for attr in tmdb_txt:
                     if attr in parsed:
-                        data[moviefoldername][(attr.title())] = (parsed[attr])
+                        data[(attr.title())] = (parsed[attr])
                     else:
-                        data[moviefoldername][(attr.title())] = "N/A"
+                        data[(attr.title())] = "N/A"
         handle.close()
 
 
@@ -68,21 +66,21 @@ def getMovieContent(folder):
             lines = handle.readline()
             lines = lines.split("\\r\\n\\r\\n")
             
-            data[moviefoldername]["Reviews"] = (lines)
+            data["Reviews"] = (lines)
 
 
             
         handle.close()
     else:
-        data[moviefoldername]["Reviews"] = "N/A"
+        data["Reviews"] = "N/A"
 
 
 
     
 
 
-    if data[moviefoldername]["Overview"] == data[moviefoldername]["Plot"]:
-        del data[moviefoldername]["Overview"]
+    if data["Overview"] == data["Plot"]:
+        del data["Overview"]
 
 
     formattedData = doubleQuoteDict(data)
@@ -93,18 +91,21 @@ def getMovieContent(folder):
     
     
 
-rootdir = 'movies'
+rootdir = 'Projeto/movies'
 for file in os.listdir(rootdir):
     d = os.path.join(rootdir, file)
     if os.path.isdir(d):
         auxDict = getMovieContent(d)
-        for key, value in auxDict.items():
-            finalData[key] = value
+        finalData.append(auxDict)
+        
+            
+        
         
 
 #print(finalData)
 #dd = (getMovieContent("movies/MonstersInc"))
 #print(dd)
-with open('data.json', 'w') as fp:
-    json.dump(finalData, fp)
+with open('Projeto/data.json', 'w') as fp:
+    fp.write(str(finalData))
+
 
